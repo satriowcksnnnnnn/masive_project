6be53ef206ef6165285e79c545b9833f0c182d35
satriowcksnnnnnn <132806616+satriowcksnnnnnn@users.noreply.github.com>
2024-05-23 09:08:59 +0700

    Revert "Style"

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,15 +10,15 @@ export default function Hero() {
         height: '100vh',
         alignItems: 'center',
         justifyContent: 'center',
-    };
+     };
     return (
     <div>  
         <div style={containerStyle}>
-            <div className='flex flex-col justify-center items-center pt-80'>
-                <h1 className='text-third font-semibold text-5xl container mx-auto px-60 text-center'>
-                    JELAJAHI KREATIVITAS DENGAN KOLEKSI GALERI KAMI
+            <div className='flex flex-col justify-center items-center pt-[180px]'>
+                <h1 className='text-third font-semibold text-[45px] pt-[166px] ml-20'>
+                    Jelajahi kreativitas dengan koleksi galeri kami
                 </h1>
-                <p className='text-white text-base text-center mt-4 container mx-auto px-52'>
+                <p className='text-white font-medium text-base w-[567px] h-[72px] ml-20 text-center'>
                     Pada platform kami ini akan menampilkan banyak sekali edukasi seni untuk anda dapat pelajari lebih detail. Dan tentunya menjadi platform yang selalu update, bagi kebutuhan anda
                 </p>   
             </div>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,10 +4,6 @@ import SignIn from "./Daftar/SignIn";
 import SignUp from "./Masuk/SignUp";
 import Learn from './Pelajari/Learn'
 import Colection from './Koleksi/Colection';
-import Upload from './Upload/Upload'
-import News from './Berita/News';
-import Schedule from './Agenda/Schedule';
-import Review from './Ulasan/Review';
 
 function App() {
 
@@ -19,10 +15,6 @@ function App() {
         <Route path='/masuk' element={<SignUp/>}/>
         <Route path='/pelajari' element={<Learn/>}/>
         <Route path='/koleksi' element={<Colection/>}/>
-        <Route path='/upload' element={<Upload />}/>
-        <Route path='/berita' element={<News />}/>
-        <Route path='/agenda' element={<Schedule />}/>
-        <Route path='/ulasan' element={<Review  />}/>
       </Routes>
     </Router>
     
