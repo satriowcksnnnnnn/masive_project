68c4b4d0009311eb698d61d8cd5ba2651a3b7f98
satriowcksnnnnnn <satriobaguswicaksono28@gmail.com>
2024-05-24 11:08:34 +0700

    page-berita

--- src/component/Berita.jsx
+++ src/component/Berita.jsx
@@ -1,18 +1,90 @@
 import React from 'react';
 import Beritaimg1 from '../assets/image/Beritaimg1.png'
+import Beritaimg2 from '../assets/image/Beritaimg2.png'
+import Profil1 from '../assets/image/profil1.jpg'
 
 export default function App() {
   return (
-    <div className='bg-black w-full min-h-screen '>
+    <div className='bg-black w-full min-h-screen px-20 pt-5 pb-[72px]'>
       <div className='p-4 '>
         <div className='px-[100px] mt-[130px]'>
         <h1 className='text-white text-[40px] '>
           Kabar Terbaru
         </h1>
-        <img src={Beritaimg1}/>
-        <p className='text-white mt-5'>
-          Pertunjukan Wayang Orang Berjudul ”Pandu Dewanata”
-        </p>
+        <div className='flex gap-14'>
+          <div className='max-w-72'>
+              <img src={Beritaimg1}/>
+              <h3 className='text-white mt-5 font-bold text-xl'>
+                Pertunjukan Wayang Orang Berjudul ”Pandu Dewanata”
+              </h3>
+              <p className='text-base text-white mt-3 text-justify'>
+                Berkisah tentang Pandhu Dewanata yang memiliki permaisuri Dewi Kunthi dan Dewi Madrim. <span className='text-[10px] text-[#999999]'>12 hari yang lalu</span> 
+              </p>
+              <div className='flex gap-3 items-center mt-5 mb-3'>
+                <img src={Profil1} className='rounded-full w-10 h-10'/>
+                <div className='text-white'>
+                    <p className='text-xs'>
+                      Khairunnisa Mukinin
+                    </p>
+                    <p className='text-[10px]'> 
+                      Detik.com
+                    </p>
+                </div>
+              </div>
+                    <svg width="289" height="2" viewBox="0 0 289 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="1" x2="289" y2="1" stroke="#787777" stroke-width="2"/>
+                    </svg>
+              <div className='text-white'>
+                <h1 className='text-2xl font-medium mb-6 mt-[14px]'>
+                  Photography
+                </h1>
+                <p className='text-base font-medium text-justify'>
+                  Sang seniman di balik terciptanya lukisan tersebut yakni Age Airlangga yang akrab dipanggil Tutu. Untuk meriahkan pameran GBK Unfolds, Tutu memberikan karya terbaiknya yang ia lukis pada tahun 2024.
+                </p>
+              </div>
+          </div>
+          <div className='text-white max-w-xl'>
+              <img src={Beritaimg2}/>
+              <h2 className='text-white text-2xl font-semibold text-center mt-[14px] mb-[42px]'>
+                Teater Jangkar Bumi Pentaskan Pertunjukan Berjudul    “Dukun-Dukunan”
+              </h2>
+              <p className='text-base text-justify'>
+              Teater Jangkar Bumi didukung oleh www.indonesiakaya.com menggelar pentas berjudul “Dukun-Dukunan” pada tanggal 16 Februari 2019 di Gedung Teater Arena Taman Budaya Jawa Tengah, Jl. Ir. Sutami, Jebres Kota Surakarta. Teater yang berasal dari Madrasah Aliyah (MA) Qudsiyyah Kudus ini pernah meraih gelar Teater Terbaik I dalam ajang Teater Djarum Award – Festival Teater Pelajar ke-11 (FTP-XI) tahun 2018 yang diselenggarakan oleh Teater Djarum bersama Departemen Pendidikan Pemuda dan Olahraga Kabupaten Kudus.
+              </p>
+          </div>
+          <div className='max-w-72'>
+              <img src={Beritaimg1}/>
+              <h3 className='text-white mt-5 font-bold text-xl'>
+                Pertunjukan Wayang Orang Berjudul ”Pandu Dewanata”
+              </h3>
+              <p className='text-base text-white mt-3 text-justify'>
+                Berkisah tentang Pandhu Dewanata yang memiliki permaisuri Dewi Kunthi dan Dewi Madrim. <span className='text-[10px] text-[#999999]'>12 hari yang lalu</span> 
+              </p>
+              <div className='flex gap-3 items-center mt-5 mb-3'>
+                <img src={Profil1} className='rounded-full w-10 h-10'/>
+                <div className='text-white'>
+                    <p className='text-xs'>
+                      Khairunnisa Mukinin
+                    </p>
+                    <p className='text-[10px]'> 
+                      Detik.com
+                    </p>
+                </div>
+              </div>
+                    <svg width="289" height="2" viewBox="0 0 289 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="1" x2="289" y2="1" stroke="#787777" stroke-width="2"/>
+                    </svg>
+              <div className='text-white'>
+                <h1 className='text-2xl font-medium mb-6 mt-[14px]'>
+                  Photography
+                </h1>
+                <p className='text-base font-medium text-justify'>
+                  Sang seniman di balik terciptanya lukisan tersebut yakni Age Airlangga yang akrab dipanggil Tutu. Untuk meriahkan pameran GBK Unfolds, Tutu memberikan karya terbaiknya yang ia lukis pada tahun 2024.
+                </p>
+              </div>
+          </div>
+        </div>
+        
         </div>
       </div>
     </div>
