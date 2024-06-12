a4f54263e3d3032a8c2a20eaa789ad91c57f4e7c
satriowcksnnnnnn <satriobaguswicaksono28@gmail.com>
2024-06-12 22:13:02 +0700

    update-admin

--- frontend/src/component/NavbarAdmin.jsx
+++ frontend/src/component/NavbarAdmin.jsx
@@ -42,9 +42,10 @@ export default function Navbar() {
                     <img src='/images/Logo.png' alt="Logo" width={50} />
                 </div>
                 <div className="flex-auto flex justify-center items-center gap-5 text-sm">
-                    <Link to="/koleksi" className={isActive ? 'text-third' : ''}>Koleksi</Link>
+                    <Link to="/koleksi" className={isActive ? 'text-third' : ''}>Karya</Link>
                     <Link to='/agenda' className={activeRoute === '/agenda' ? 'text-third' : ''}>Agenda</Link>
                     <Link to='/berita' className={activeRoute === '/berita' ? 'text-third' : ''}>Berita</Link>
+                    <Link to='/upload' className={activeRoute === '/upload' ? 'text-third' : ''}>Seniman</Link>
                 </div>
                 <div className="flex-initial w-60 flex items-center gap-6 justify-end">
                     <button onClick={() => navigate('/masuk')} className='bg-white px-[18px] py-2 rounded-[9px] hover:bg-gray-300 text-black ease-in-out duration-200 font-semibold'>
